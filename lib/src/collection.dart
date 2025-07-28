4d2251bef3ec5f1668cfb01a010ad6f9ada9c34e
SneakyGraySnake <tusiimejohnmark@gmail.com>
2025-07-28 08:45:48 -0700

    Update collection.dart

--- lib/src/collection.dart
+++ lib/src/collection.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
-
 import 'models/DistanceDocSnapshot.dart';
 import 'point.dart';
 import 'util.dart';
@@ -23,9 +21,7 @@ class GeoFireCollectionRef {
   /// return the Document mapped to the [id]
   Stream<List<DocumentSnapshot<Map<String, dynamic>>>> data(String id) {
     return _stream.map((querySnapshot) {
-      return querySnapshot.docs
-          .where((doc) => doc.id == id)
-          .toList();
+      return querySnapshot.docs.where((doc) => doc.id == id).toList();
     });
   }
 
@@ -35,7 +31,7 @@ class GeoFireCollectionRef {
       final colRef = _collectionReference as CollectionReference<Map<String, dynamic>>;
       return colRef.add(data);
     } catch (e) {
-      throw Exception('cannot call add on Query, use collection reference instead');
+      throw Exception('Cannot call add on Query, use collection reference instead');
     }
   }
 
@@ -45,17 +41,17 @@ class GeoFireCollectionRef {
       final colRef = _collectionReference as CollectionReference<Map<String, dynamic>>;
       return colRef.doc(id).delete();
     } catch (e) {
-      throw Exception('cannot call delete on Query, use collection reference instead');
+      throw Exception('Cannot call delete on Query, use collection reference instead');
     }
   }
 
-  /// create or update a document with [id], [merge] defines whether the document should overwrite
+  /// create or update a document with [id]
   Future<void> setDoc(String id, Map<String, dynamic> data, {bool merge = false}) {
     try {
       final colRef = _collectionReference as CollectionReference<Map<String, dynamic>>;
       return colRef.doc(id).set(data, SetOptions(merge: merge));
     } catch (e) {
-      throw Exception('cannot call set on Query, use collection reference instead');
+      throw Exception('Cannot call set on Query, use collection reference instead');
     }
   }
 
@@ -66,7 +62,7 @@ class GeoFireCollectionRef {
       final point = GeoFirePoint(latitude, longitude).data;
       return colRef.doc(id).set({'$field': point}, SetOptions(merge: true));
     } catch (e) {
-      throw Exception('cannot call set on Query, use collection reference instead');
+      throw Exception('Cannot call set on Query, use collection reference instead');
     }
   }
 
@@ -79,16 +75,12 @@ class GeoFireCollectionRef {
   }) {
     final precision = Util.setPrecision(radius);
     final centerHash = center.hash.substring(0, precision);
-    final area = Set<String>.from(
-      GeoFirePoint.neighborsOf(hash: centerHash)..add(centerHash),
-    ).toList();
+    final area = Set<String>.from(GeoFirePoint.neighborsOf(hash: centerHash)..add(centerHash)).toList();
 
     final queries = area.map((hash) {
       final tempQuery = _queryPoint(hash, field);
       return _createStream(tempQuery)!.map((querySnapshot) {
-        return querySnapshot.docs
-            .map((element) => DistanceDocSnapshot(element, null))
-            .toList();
+        return querySnapshot.docs.map((element) => DistanceDocSnapshot(element, null)).toList();
       });
     });
 
@@ -96,25 +88,40 @@ class GeoFireCollectionRef {
 
     return mergedObservable.map((list) {
       final mappedList = list.map((distanceDocSnapshot) {
-        final fieldList = field.split('.');
         final snapData = distanceDocSnapshot.documentSnapshot.data();
-        var geoPointField = snapData[fieldList[0]];
+
+        if (snapData == null) return distanceDocSnapshot; // ✅ Fix for null data
+
+        final fieldList = field.split('.');
+        dynamic geoPointField = snapData[fieldList[0]];
+
         for (int i = 1; i < fieldList.length; i++) {
-          geoPointField = geoPointField[fieldList[i]];
+          if (geoPointField is Map && geoPointField.containsKey(fieldList[i])) {
+            geoPointField = geoPointField[fieldList[i]];
+          } else {
+            geoPointField = null;
+            break;
+          }
         }
-        final GeoPoint geoPoint = geoPointField['geopoint'];
-        distanceDocSnapshot.distance =
-            center.distance(lat: geoPoint.latitude, lng: geoPoint.longitude);
+
+        if (geoPointField != null &&
+            geoPointField is Map &&
+            geoPointField['geopoint'] is GeoPoint) {
+          final GeoPoint geoPoint = geoPointField['geopoint'];
+          distanceDocSnapshot.distance = center.distance(
+            lat: geoPoint.latitude,
+            lng: geoPoint.longitude,
+          );
+        }
+
         return distanceDocSnapshot;
       });
 
       final filteredList = strictMode
-          ? mappedList
-              .where((doc) => doc.distance! <= radius * 1.02)
-              .toList()
+          ? mappedList.where((doc) => doc.distance != null && doc.distance! <= radius * 1.02).toList()
           : mappedList.toList();
 
-      filteredList.sort((a, b) => (a.distance! * 1000).toInt() - (b.distance! * 1000).toInt());
+      filteredList.sort((a, b) => (a.distance ?? 0 * 1000).toInt() - (b.distance ?? 0 * 1000).toInt());
 
       return filteredList.map((e) => e.documentSnapshot).toList();
     });
@@ -135,29 +142,21 @@ class GeoFireCollectionRef {
     required String field,
     bool strictMode = false,
   }) {
-    return _buildQueryStream(center: center, radius: radius, field: field, strictMode: strictMode)
-        .asBroadcastStream();
+    return _buildQueryStream(center: center, radius: radius, field: field, strictMode: strictMode).asBroadcastStream();
   }
 
-  Stream<List<DistanceDocSnapshot>> mergeObservable(
-      Iterable<Stream<List<DistanceDocSnapshot>>> queries) {
+  Stream<List<DistanceDocSnapshot>> mergeObservable(Iterable<Stream<List<DistanceDocSnapshot>>> queries) {
     return Rx.combineLatest(queries, (List<List<DistanceDocSnapshot>> lists) {
       return lists.expand((x) => x).toList();
     });
   }
 
-  /// construct a query for the [geoHash] and [field]
   Query<Map<String, dynamic>> _queryPoint(String geoHash, String field) {
     final end = '$geoHash~';
-    return _collectionReference
-        .orderBy('$field.geohash')
-        .startAt([geoHash])
-        .endAt([end]);
+    return _collectionReference.orderBy('$field.geohash').startAt([geoHash]).endAt([end]);
   }
 
-  Stream<QuerySnapshot<Map<String, dynamic>>>? _createStream(
-      Query<Map<String, dynamic>> ref) {
+  Stream<QuerySnapshot<Map<String, dynamic>>>? _createStream(Query<Map<String, dynamic>> ref) {
     return ref.snapshots();
   }
 }
-
